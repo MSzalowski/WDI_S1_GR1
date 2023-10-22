f1da63089eec8bcfe9d3481af6825263d6eb3754
Michał Szałowski <michal.szalowski@gmail.com>
2023-10-22 12:15:06 +0200

    add task1.py

---
+++ Set1/task1.py
@@ -0,0 +1,27 @@
+def GCD(a, b):
+  if a == 0:
+      return b
+  elif b == 0:
+      return a
+  elif a >= b:
+      return GCD(a % b, b)
+  else:
+      return GCD(a, b % a)
+
+def LCM(a, b):
+  if (a == 0 or b == 0):
+    return 0
+  return a * b / GCD(a, b)
+
+# Tests
+# GCD
+assert(GCD(0, 0) == 0)
+assert(GCD(0, 1) == 1)
+assert(GCD(1, 0) == 1)
+assert(GCD(1, 1) == 1)
+
+assert(GCD(2, 2) == 2)
+assert(GCD(2, 4) == 2)
+assert(GCD(4, 2) == 2)
+
+assert(GCD(124, 48) == 4)
